90f0e4b2f3e8aa4d6e83ce4e5cba2734ac317b4a
gp <776182719@qq.com>
2020-02-10 11:18:38 +0800

    updatecode

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,8 +3,8 @@
 const commonUrl = {
   // baseUrl: '/api' // 为什么生产环境的时候是/api呢？看下面的操作
   // baseUrl: 'http://192.168.1.77:8092',//xh
-  baseUrl:'http://115.28.128.187:8092', //外2
-
+  // baseUrl:'http://115.28.128.187:8092', //外2
+  baseUrl:'http://192.168.1.112:8092'//hy
 }
 
 export default commonUrl
